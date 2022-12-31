382bba08b6fe667dd66d52fa6c80b5d8bdb40a16
Reneeeeeeeeeeee <110330411+Reneeeeeeeeeeee@users.noreply.github.com>
2023-01-01 00:23:54 +0800

    issue fixed

--- static/index.js
+++ static/index.js
@@ -486,7 +486,7 @@
         })
 
     
-    const form= document.getElementById('form');
+    const form= document.querySelector('.form');
     form.addEventListener('submit', event=>{
         event.preventDefault();
         const formData= new FormData(form);
